302af00796f1af05af18384e816fe33a26ea9274
amishaMgupta <amishaagarwal182@gmail.com>
2026-05-21 12:29:05 +0530

    Login in the html

--- ExpenseManagerApp/backend/app.js
+++ ExpenseManagerApp/backend/app.js
@@ -4,7 +4,27 @@ const port = 3000
 
 // call index.html as first page loaded
 app.use(express.static('public'))
+app.use(express.json())
 
+// get user name and password from UI for Login
+app.get("/users",async(req,res)=>{
+    mydata = req.query;
+    console.log(mydata.username);
+    let messager = {"message":"failed"};
+   await fetch("http://localhost:3001/users")
+    .then(res=>res.json())
+    .then(data=>{
+
+       data.map(ele=>{
+        if(mydata.username == ele.userName && mydata.password == ele.password){
+            console.log("inside if");
+            message= {"message":"successfull"};
+           }
+        })
+       });
+      res.send(message);
+      
+    })
 app.listen(port,()=>{
     console.log(`Server is running on port:- ${port}`)
 })
